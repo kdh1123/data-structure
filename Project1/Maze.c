3808552227d8637a0486cfd053d99e04da0f5884
yoone1123 <81676485+yoone1123@users.noreply.github.com>
2021-09-16 15:49:58 +0900

    ...

--- Project1/Maze.c
+++ Project1/Maze.c
@@ -1,7 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int main() {
-    int m[9][9] = {
+int m[9][9] = {
         {2,2,2,2,2,2,2,2,2},
              {2,0,0,0,0,0,0,0,2},
              {2,0,2,2,0,2,2,0,2},
@@ -11,10 +10,55 @@ int main() {
              {2,2,0,2,2,0,2,2,2},
              {2,0,0,0,0,0,0,0,2},
              {2,2,2,2,2,2,2,2,2}
-    };
-    int Si, Sj, Ei, Ej;
-    int success, top, path_i[100], path_j[100];
-    //재귀함수는 시스템 스택
-    //S:start,출발점 E:End,도착점, top 경로 배열 인덱스
-    //Path_i, Path_j: 지나간 길, 즉 행과 열을 저장함(i는 행, j는 열)
+};
+int Si, Sj, Ei, Ej;
+int success, top, path_i[100], path_j[100];
+//재귀함수는 시스템 스택
+//S:start,출발점 E:End,도착점, top 경로 배열 인덱스
+//Path_i, Path_j: 지나간 길, 즉 행과 열을 저장함(i는 행, j는 열)
+void printPath();
+int visit(int i , int j);
+int main() {
+    top = 0;
+    success = 0;
+    printf("=======미로 탐색 경로 찾기=======\n");
+
+    Si = 1; Sj = 1; Ei = 7; Ej = 7;
+
+    if (visit(Si, Sj) == 0)printf("미로의 출구를 찾을 수 없음");
+    else printf("모든 미로의 출구를 찾음");
+}
+void printPath() {//미로에서 찾은 길을 출력하는 함수
+    for (int i = 0; i < top; i++) {
+        printf(" [%d][%d] ", path_i[i], path_j[i]);
+    }
+    printf("\n");
+}
+int visit(int i, int j) {
+    static int pathcount = 1;//찾은 경로의 수를 카운트
+    m[i][j] = 1;//지나갈 길에 1로 세팅, 지나갈 수 있는 길은 0인 경우임
+    path_i[top] = i;
+    path_j[top++] = j;
+    //만약 도착점에 도착했을 때,
+    if (i == Ei && j == Ej) {
+        success = 1;
+        printf("찾은 경로 %d번\n", pathcount++);
+        printPath();
+    }
+    //미로 탐색 코드
+    if (m[i][j + 1] == 0) {
+        visit(i, j+1);
+    }
+    if (m[i+1][j] == 0) {
+        visit(i+1, j);
+    }
+    if (m[i][j - 1] == 0) {
+        visit(i, j - 1);
+    }
+    if (m[i-1][j] == 0) {
+        visit(i-1, j);
+    }
+    top--;
+    m[i][j] = 0;
+    return success;
 }
--- Project1/스택과 큐.c
+++ Project1/스택과 큐.c
@@ -2,51 +2,73 @@
 #include<stdio.h>
 #define SIZE 5
 //큐
-void menu() {
-	printf("\n-----------------------------------------------\n");
-	printf("1번 큐에 삽입(push) 2번 큐에서 삭제(pop)\n");
-	printf("3번 내용 보기(display) 4번 프로그램 종료(exit)\n");
-	printf("\n-----------------------------------------------\n");
+//Queue 구현
+//FIFO, front, back, Enqueue, Dequeue
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int queue[5];
+
+
+void menuf() { //메뉴 함수 function
+	printf("\n1.큐에 삽입 2.큐에서 삭제\n");
+	printf("3.내용 보기 4.종료\n");
 }
-int queue[SIZE];
-void enqueue(int* back, int item) {//큐에 데이터 삽입
-	if (*back == 4) {
+
+
+//넣는 값이 item
+void Enqueue(int* back, int item) {//큐에 삽입
+	if (*back == 4) {//현재 큐의 사이즈는 5
 		printf("큐가 가득참\n");
 		return;
 	}
-	queue[++ * back]= item;
+	queue[++ * back] = item;
 }
-int dequeue(int * front, int back) {
+int Dequeue(int* front, int back) {
 	if (*front == back) {
-		//큐가 비었음
-		return -1;
+		return -1; //-1이면 큐가 비었다고 생각
+		//보통 0이 리턴되면 정상적인 종료
 	}
 	return queue[++ * front];
 }
+void Display(int front, int back) {
+	if (front == back)
+		printf("큐가 비었음\n");
+	for (int i = front; i < back + 1; i++)
+		printf("%4d", queue[i]);
+}
+
+
 int main() {
-	int val, n;
-	printf("*****큐 구현 메뉴*****\n");
+	//큐, 스택 모두 1차원 배열로 구현
+	int front = -1, back = -1;
+	//queue : front, back // stack : top
+	int menu, item;
 	while (1) {
-		menu();
-		printf("\n메뉴를 선택하세요 : \n"); scanf("%d", &n);
-		switch (n) {
-		case 1://push
-			printf("큐에 삽입할 숫자 :"); scanf("%d", &n);
-			enqueue(n);
+		menuf();
+		scanf("%d", &menu);
+		switch (menu) {
+		case 1://큐에 데이터 또는 job 삽입
+			scanf("%d", &item);
+			Enqueue(&back, item);
 			break;
-		case 2://pop
-			dequeue();
+		case 2://큐에서 삭제
+			item = Dequeue(&front, back);
+			if (item == -1)
+				printf("큐가 비었음");
+			else
+				printf("%d가 삭제됨\n", item);
 			break;
-		case 3://display
-			display();
+		case 3://큐의 내용 보여주기
+			Display(front, back); //과제
+			//큐의 내용을 출력해 보시오
 			break;
-		case 4://exit
+		case 4://프로그램 종료
 			return 0;
-			break;
 		default:
-			printf("다시 입력하세요");
+			printf("다시 입력하세요.");
 		}
 	}
+
 
 }
 //int stack[SIZE];
