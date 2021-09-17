be282167c7571bb7c7f5ee685766f8c07f4462c0
yoone1123 <81676485+yoone1123@users.noreply.github.com>
2021-09-17 12:26:57 +0900

    ...

---
+++ Project1/쉘 정렬.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+void ShellSort(int arr[],int n) {
+	for (int gap = n / 2; gap > 0; gap /= 2) {
+		for (int i = gap; i < n; i++) {
+			int tmp = arr[i];
+			int j;
+			for (j = 1; j >= gap && arr[j - gap] > tmp; j -= gap) {
+				arr[j] = arr[j - gap];
+			}
+			arr[j] = tmp;
+		}
+	}
+}
+int main() {
+	int list[]={1,9,1,5,1,2,3,7,5,8,11,2};
+	ShellSort(list, sizeof(list)/sizeof(int));
+	for (int i = 0; i < sizeof(list) / sizeof(int); i++) {
+		printf("[%d] ", list[i]);
+	}
+}
