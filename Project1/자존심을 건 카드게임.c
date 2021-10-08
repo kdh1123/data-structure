053fa7bd1b5658e3298070ae0a9e750dc1c93b11
yoone1123 <81676485+yoone1123@users.noreply.github.com>
2021-10-08 09:35:05 +0900

    카드게임 수정

--- Project1/자존심을 건 카드게임.c
+++ Project1/자존심을 건 카드게임.c
@@ -3,29 +3,35 @@
 void Card();
 int main() {
 	int n;
-	char card[20][20], A, B;
-	printf("카드 개수 입력 : ");
+	char card[20][20], A[2];
+	printf("알파벳 두 개 : ");
+	scanf(" %c %c", &A[0],&A[1]);
+	printf("영단어 개수 입력 : ");
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
 		printf("영단어 입력 : ");
 		scanf(" %s", card[i]);
 	}
-	printf("A가 고른 알파벳 : ");
-	scanf(" %c", &A);
-	printf("B가 고른 알파벳 : ");
-	scanf(" %c", &B);
-	if (A == B) return 0;
-	Card(n,card,A,B);
+	if (A[0] == A[1]) return 0;
+	Card(n, card, A);
 }
-void Card(int n, char c[][20],char A,char B) {
+void Card(int n, char c[][20],char A[]) {
+	int cnt=0,len;
 	for (int i = 0; i < n; i++) {
-		if (c[i][0] == A || c[i][0] == A + 32) {
-			printf("A 승리"); return 0;
+		for (int j = 0;; j++) {
+			if (c[i][j] == '\0') {
+				len = j-1; break;
+			}
 		}
-		else if (c[i][0] == B || c[i][0] == B + 32) {
-			printf("B 승리"); return 0;
+		if ((c[i][0] == A[0] || c[i][0] == A[0] + 32)&& (c[i][len] == A[1] || c[i][len] == A[1] + 32)) {
+			cnt++;
+		}
+		else if ((c[i][0] == A[1] || c[i][0] == A[1] + 32) && (c[i][len] == A[0] || c[i][len] == A[0] + 32)) {
+			cnt++;
 		}
 	}
-	printf("%c %c\n\n", A+32, B+32);
-	printf("무승부");
+	if (cnt == 0)
+		printf("해당되는 영단어가 없음\n");
+	else
+		printf("%d", cnt);
 }
